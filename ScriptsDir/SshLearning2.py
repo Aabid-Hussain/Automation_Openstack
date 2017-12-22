ffa36e71ea3344787fdcfd67de8b2b9b7a67f860
Aabid-Hussain <aabidhussain.inc@gmail.com>
2017-12-22 17:17:30 +0530

    designed coding logic and it's function. working on writing the code for the same.

--- ScriptsDir/SshLearning2.py
+++ ScriptsDir/SshLearning2.py
@@ -69,78 +69,64 @@ class SSHAutomation:
     def close_connection_host(self):
 
         '''
-        define close_connection_host(self)
-    - use try & except block for capturing any exception
-    - close the open connection using close method
-    - assign connection attribute to None
-
+        -define close_connection_host(self)
+        - use try & except block for capturing any exception
+        - close the open connection using close method
+        - assign connection attribute to None
         '''
-
-
-
         pass
 
 
     def command_required_root_privilage(self):
         '''
-                define command_required_root_privilege(self,
+        define command_required_root_privilege(self,
                 set_password=False, password='root',
                 first_command, second_command, username='default'
                 )
-             -invoke_shell() and send following command
-                - send "sudo su - \n" and sleep for 2secs
-                - send password to enable sudo and sleep for 2secs
-                - send first_command and sleep for 2secs
-                - send second_command and sleep for 2secs
-                - check set_password if True then
-                    -change the password using command "passwd username"
-                    -send password and sleep for 2 secs
-                    -send confirmation password again and sleep for 2secs
-            - check if channel is ready to receive data else sleep for 5secs
-            - receive the data and store it in output variable
-            - finally decode the output in utf-8 or ascii and print or log it.
-
-                :param first_command:
-                :param second_command:
-                :param username:
-                :param set_password:
-                :param password:
-                :return:
-                '''
+        -invoke_shell() and send following command
+            - send "sudo su - \n" and sleep for 2secs
+            - send password to enable sudo and sleep for 2secs
+            - send first_command and sleep for 2secs
+            - send second_command and sleep for 2secs
+            - check set_password if True then
+                -change the password using command "passwd username"
+                -send password and sleep for 2 secs
+                -send confirmation password again and sleep for 2secs
+        - check if channel is ready to receive data else sleep for 5secs
+        - receive the data and store it in output variable
+        - finally decode the output in utf-8 or ascii and print or log it.
+        '''
 
         pass
 
     def command_required_user_privilage(self):
         '''
-                define command_required_user_privilege(self, command,
-                                sudo=False)
-                -if user provide commands requires sudo permission then we need to pass password.
-                - one way to deal this problem is using
+        define command_required_user_privilege(self, command, sudo=False)
+            -if user provide commands requires sudo permission then we need to pass password.
+            - one way to deal this problem is using
                 sudo -S -p '' sudo ls -ltr
-                -p: prompt, use a custom password prompt with optional escape sequence[%].
-                -S: --stdin, write the prompt to the standard error
+            -p: prompt, use a custom password prompt with optional escape sequence[%].
+            -S: --stdin, write the prompt to the standard error
                  and read the password from the standard input
                  instead of using the terminal device.
                  The password must be followed by a newline character.
-                - use exec_command(command) to store stdin, stdout & stderr
-                - if password exist the write the password to stdin and then flush once it is used.
-                    return(stdout.readlines(), stderr.readlines())
-
-                '''
+            - use exec_command(command) to store stdin, stdout & stderr
+            - if password exist the write the password to stdin and then flush once it is used.
+            - return(stdout.readlines(), stderr.readlines())
+        '''
 
         pass
 
     def write_local_conf_data(self):
         '''
-        define write_local_conf_data(self, filename, data)
-    - open SFTPClient session using "open_sftp()"
-    - change directory to devstack using chdir()
-    - for time being, if error out, mkdir(devstack) and filepath='devstack/'+filename
-    - open file in write mode and then write data.
-    -use try and except to capture exception properly.
-
-
+        - define write_local_conf_data(self, filename, data)
+        - open SFTPClient session using "open_sftp()"
+        - change directory to devstack using chdir()
+        - for time being, if error out, mkdir(devstack) and filepath='devstack/'+filename
+        - open file in write mode and then write data.
+        - use try and except to capture exception properly.
         '''
+
         pass
 
 
