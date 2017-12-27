39656b3bc0715064461c7d6afb1f5f116f70fe91
Aabid-Hussain <aabidhussain.inc@gmail.com>
2017-12-27 12:05:29 +0530

    working on code to verify if a package is installed. If not install it.

---
+++ package_installation_check.py
@@ -0,0 +1,39 @@
+import sys
+import pip
+import time
+
+
+def package_installation_check(package):
+
+    try:
+        import package
+        return True
+
+    except:
+        return False
+
+def package_install(package):
+
+    if package_installation_check(package):
+        print("{} Package is already installed!".format(package))
+
+    else:
+
+        pip.main(["install", package])
+        time.sleep(2)
+        sys.exit()
+
+
+def package_uninstall(package):
+    if package_installation_check(package):
+        #for debugging: pip.main(["uninstall", package, "-vv"])
+        pip.main(["uninstall", package])
+        time.sleep(2)
+        sys.exit()
+    else:
+        pass
+
+if __name__ == '__main__':
+
+
+#pip install -e git+https://github.com/paramiko/paramiko/#egg=paramiko
